64a5bce2361bb7b8a554bf4b3eeb585d0acea045
Sarah Alexander <sjalex78@gmail.com>
2022-07-26 17:17:56 +1000

    adding a test file

---
+++ Courses/2022_07_Vitest/basic-testing-frontend/src/math.test.js
@@ -0,0 +1,7 @@
+import { it } from 'vitest';
+// test can be interchangeable with it
+
+it('should summarize all the number values in an array', () => {
+    
+});
+// in the string you describe/statement what your expected behaviour is
