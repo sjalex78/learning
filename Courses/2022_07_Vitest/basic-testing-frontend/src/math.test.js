15f1c7f35f63e1ee6d556494a43fe9688dd1cae3
Sarah Alexander <sjalex78@gmail.com>
2022-07-27 10:33:39 +1000

    writing more test to include invalid inputs

--- Courses/2022_07_Vitest/basic-testing-frontend/src/math.test.js
+++ Courses/2022_07_Vitest/basic-testing-frontend/src/math.test.js
@@ -4,8 +4,45 @@ import { expect, it} from 'vitest';
 import { add } from './math';
 
 it('should sum all the numbers values in an array', () => {
-    const result = add([1,2,3]);
-    expect(result).toBe(6);
+    //arrange
+    const numbers = [1, 2, 3];
+    const expectedResult =numbers.reduce(
+        (preValue, curValue) => preValue +curValue,
+        0
+    );
+
+    // act
+    const result = add(numbers);
+
+    //assert
+    expect(result).toBe(expectedResult);
+    //rather than hard code the expected results (6) calculate the expected result reduce method that can be used on arrays.
 });
 
 // in the string you describe/statement what your expected behaviour is
+
+it('should yield NaN is at least one invalid number is provided',() =>{
+    //arrange
+    const inputs = [1, 'invalid'];
+
+    //act
+    const result = add(inputs);
+
+    //assert
+    expect(result).toBeNaN();
+});
+
+it('should yield correct sum if array of numeric string values is provided', () => {
+    //arrange
+    const numbers = ['1', '2'];
+    const expectedResult =numbers.reduce(
+        (preValue, curValue) => +preValue + +curValue,
+        0
+    );
+
+    //act
+    const result = add(numbers);
+
+    //assert
+    expect(result).toBe(expectedResult);
+});
