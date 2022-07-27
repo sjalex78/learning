98bb6ad8c81db4f3da6e5e31fd01c0cb2597c2f9
Sarah Alexander <sjalex78@gmail.com>
2022-07-27 10:47:55 +1000

    writing test to throw errors in different situations

--- Courses/2022_07_Vitest/basic-testing-frontend/src/math.test.js
+++ Courses/2022_07_Vitest/basic-testing-frontend/src/math.test.js
@@ -46,3 +46,36 @@ it('should yield correct sum if array of numeric string values is provided', () 
     //assert
     expect(result).toBe(expectedResult);
 });
+
+//check if the function yields 0 if the array is empty
+it('should return zero with an empty array', () => {
+    //arrange
+    const numbers = [];
+
+    //act
+    const result = add(numbers);
+    //assert
+    expect(result).toBe(0);
+});
+
+
+//check if no argument is added should through an error
+//when you wrap a function in the constant it will look to see if there was an error thrown
+it('should throw an error if no values is passed into the function', () =>{
+   const resultFn = () =>{
+    add();
+   };
+    expect(resultFn).toThrow();
+    //add .not check for the opposite added before the .toThrow
+});
+
+it('throw an error if provide with multiple arguments instead of an array', () =>{
+    const num1 =1;
+    const num2 =2;
+
+    const resultFn = () =>{
+        add(num1,num2);
+    };
+
+    expect(resultFn).toThrow();
+});
