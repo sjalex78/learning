abf92c2546db6f12676deb25e5104f65d47e68be
Sarah Alexander <sjalex78@gmail.com>
2022-08-01 09:16:07 +1000

    Add course supplied tests

--- Courses/2022_07_Vitest/basic-testing-frontend/src/util/numbers.test.js
+++ Courses/2022_07_Vitest/basic-testing-frontend/src/util/numbers.test.js
@@ -11,15 +11,20 @@ it('should take a string number and transform into a number of type number', () 
 
     //assert
     // expect(result).toBe(2);
+    // add multiple expectation only if make sense must all be true..
     expect(result).toBeTypeOf("number");
 
 });
 
 it('should yeild NAN for non-transformable values', () => {
     const input = 'invalid';
+    // empty object??
+    const input2 = {};
 
     const result =transformToNumber(input);
+    const result2 =transformToNumber(input2);
 
     expect(result).toBeNaN();
+    expect(result2).toBeNaN();
 
 });
