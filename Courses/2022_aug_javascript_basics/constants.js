7ebd2c2eac7a199a63a0bc5b37b4028c72050115
Sarah Alexander <sjalex78@gmail.com>
2022-08-03 09:36:29 +1000

    Defining constants using let or const

---
+++ Courses/2022_aug_javascript_basics/constants.js
@@ -0,0 +1,8 @@
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+//if a constant need to be reassigned use let otherwise use const
+
+const paymentRate = 0.3;
+console.log(paymentRate);
--- Courses/2022_aug_javascript_basics/run.sh
+++ Courses/2022_aug_javascript_basics/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
-
-console.log("hi");
+const { interestRate } = require ('./constants');
+console.log(interestRate);
