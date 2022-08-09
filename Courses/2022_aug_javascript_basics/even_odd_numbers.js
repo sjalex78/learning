2d77c3f907d871474012f08c3a756fd1398c6dd4
Sarah Alexander <sjalex78@gmail.com>
2022-08-09 13:58:29 +1000

    practice

--- Courses/2022_aug_javascript_basics/even_odd_numbers.js
+++ Courses/2022_aug_javascript_basics/even_odd_numbers.js
@@ -1,3 +1,7 @@
-function showNumbers(limit) {}
-
+function showNumbers(limit) {
+  for (let i = 0; i <= limit; i++) {
+    const message = i % 2 === 0 ? "even" : "odd";
+    console.log(i, message);
+  }
+}
 console.log(showNumbers(10));
