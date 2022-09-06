9ec7b6cf014763b937241d38cb527263d9f5eb76
Sarah Alexander <sjalex78@gmail.com>
2022-09-07 09:56:10 +1000

    Daily Practice for JS project
    -utilized new readme for practice flow

--- Practice/20220729_js_project/hello.test.js
+++ Practice/20220729_js_project/hello.test.js
@@ -1,13 +1,12 @@
-import { expect, it } from 'vitest';
-import { hello } from './hello.js';
+import { expect, it } from "vitest";
+import { hello } from "./hello.js";
 
-
-it('will say hello world with no name input', () => {
+it("will say hello world with no name input", () => {
   //ARRANGE
 
   //ACT
   const result = hello();
 
   //ASSERT
-  expect(result).toBe('hello world');
+  expect(result).toBe("hello world");
 });
