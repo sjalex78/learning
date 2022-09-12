1f8fac17b93467158b8b4fd0d2bca788b5b83b3d
Sarah Alexander <sjalex78@gmail.com>
2022-09-12 15:23:30 +1000

    Set up a basic form with simple validation

---
+++ Practice/20220912_formvalidator_v2/scripts.js
@@ -0,0 +1,54 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+//show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+//show input success view
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+// email valid input
+function isValidEmail(email) {
+  const regexEmail =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regexEmail.test(String(email).toLowerCase());
+}
+
+// submit
+function submitListener(event) {
+  event.preventDefault();
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+  }
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+  if (password2.value === "") {
+    showError(password2, "Confirmation Password is required");
+  } else {
+    showSuccess(password2);
+  }
+}
+//Event Listener
+form && form.addEventListener("submit", submitListener);
