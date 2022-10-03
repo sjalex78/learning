5212f4acf37dd6da002b14859d06739518f35222
Sarah Alexander <sarah.alexander@thoughtworks.com>
2022-10-03 12:25:43 +1100

    Create a test to define create a blog link

---
+++ Practice/rails_20221002_practice/spec/features/create_a_blog_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'creating blogs', js: true do
+  scenario 'new user creates a blog post' do
+    When 'user visits the app' do
+      visit root_path
+    end
+
+    Then 'they see a new article link' do
+      expect(page).to have_text('New article')
+    end
+
+    When 'user clicks the link' do
+      click_link('New article')
+    end
+
+    Then 'user successfully navigates to a new blog entry' do
+      expect(
+        page.current_url.last(13)
+      ).to eq '/articles/new'
+    end
+  end
+end
